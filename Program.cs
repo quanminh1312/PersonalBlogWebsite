0316df12ef9ad5b0296ca70fc8d962c41883cda6
[Nguyen Minh Quan] <[mnquan1312@gmail.com]>
2024-04-02 15:14:06 +0700

    fix post

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") + ";TrustServerCertificate=True"; ;
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") + ";TrustServerCertificate=True";
 builder.Services.AddDbContext<BlogContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddControllersWithViews();
